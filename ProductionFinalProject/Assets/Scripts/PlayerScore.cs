2f7e52d00d15cc3abcc16fe64b9c2b0f1a1e2464
Mr-Nobody99 <Kutzy99@gmail.com>
2019-03-06 17:37:59 -0500

    Added current and high score counters(high score is persistant across play sessions)

---
+++ ProductionFinalProject/Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerScore
+{
+    static int highScore = PlayerPrefs.GetInt("highScore");
+    static int currentScore = 0;
+
+    public static int GetHighScore()
+    {
+        return highScore;
+    }
+
+    public static void UpdateHighScore()
+    {
+        highScore = currentScore;
+        PlayerPrefs.SetInt("highScore", highScore);
+    }
+
+    public static int GetCurrentScore()
+    {
+        return currentScore;
+    }
+
+    public static void AddToCurrentScore(int x)
+    {
+        currentScore += x;
+    }
+}
---
+++ ProductionFinalProject/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text highScoreText;
+    public int highScore = 0;
+    public Text currentScoreText;
+    public int currentScore = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerScore.GetCurrentScore() > PlayerScore.GetHighScore())
+        {
+            PlayerScore.UpdateHighScore();
+        }
+        highScore = PlayerScore.GetHighScore();
+        highScoreText.text = highScore.ToString();
+        currentScore = PlayerScore.GetCurrentScore();
+        currentScoreText.text = currentScore.ToString();
+    }
+}
