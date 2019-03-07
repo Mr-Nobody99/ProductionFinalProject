df049813b5db34191808f818ec646ad131c467da
Mr-Nobody99 <Kutzy99@gmail.com>
2019-03-07 12:34:35 -0500

    Added Hub Zone, Fire Zone, Water Zone, Earth Zone, portal prefabs, portal script, Portal Shader and Materials.

---
+++ ProductionFinalProject/Assets/Scripts/Portal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    private bool hubZone;
+    [SerializeField]
+    private bool fireZone;
+    [SerializeField]
+    private bool waterZone;
+    [SerializeField]
+    private bool earthZone;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if(hubZone)
+            {
+                SceneManager.LoadScene(0);
+            }
+            if(fireZone)
+            {
+                SceneManager.LoadScene(1);
+            }
+            else if(waterZone)
+            {
+                SceneManager.LoadScene(2);
+            }
+            else if(earthZone)
+            {
+                SceneManager.LoadScene(3);
+            }
+        }
+    }
+}
