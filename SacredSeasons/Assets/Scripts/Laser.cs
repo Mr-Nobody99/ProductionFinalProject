a532934456530f5aca7b485803bbc06380c845fd
Mr-Nobody99 <Kutzy99@gmail.com>
2019-04-06 21:25:56 -0400

    Added boss laser

---
+++ SacredSeasons/Assets/Scripts/Laser.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    Transform targetBone;
+    Vector3 dir;
+
+    [SerializeField]
+    ParticleSystem Orb;
+    [SerializeField]
+    LineRenderer Beam;
+
+    public bool Activated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetBone = GameObject.Find("PlayerTarget").transform;
+        Activated = false;
+        Beam.useWorldSpace = true;
+        Beam.enabled = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay( transform.position, dir);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Activated)
+        {
+            Beam.enabled = true;
+            RotateToPlayer();
+            UpdateBeam();
+        }
+        else
+        {
+            Beam.enabled = false;
+        }
+    }
+
+    public void PlayParticles(bool play)
+    {
+        if (play)
+        {
+            Orb.Play();
+        }
+        else
+        {
+            Orb.Stop();
+            Orb.Clear();
+        }
+    }
+
+    void RotateToPlayer()
+    {
+        dir = targetBone.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(dir);
+    }
+
+    void UpdateBeam()
+    {
+        Beam.SetPosition(0, transform.position);
+        RaycastHit hit;
+        Physics.SphereCast(transform.position, 1.0f, dir, out hit);
+        Beam.SetPosition(1, hit.point);
+    }
+}
---
+++ SacredSeasons/Assets/Scripts/MonsterBossController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class MonsterBossController : MonoBehaviour
+{
+
+    NavMeshAgent NavAgent;
+    Animator AnimController;
+
+    [SerializeField]
+    GameObject Laser;
+    Laser laserScript;
+    bool beamActivated = false;
+
+    GameObject PlayerRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavAgent = this.GetComponent<NavMeshAgent>();
+        AnimController = this.GetComponent<Animator>();
+        PlayerRef = GameObject.FindGameObjectWithTag("Player");
+        laserScript = Laser.GetComponent<Laser>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //NavAgent.SetDestination(PlayerRef.transform.position);
+    }
+
+    public void BeginAttack()
+    {
+        print("Attack Triggered!");
+        laserScript.PlayParticles(true);
+    }
+
+    public void ActivateBeam()
+    {
+        laserScript.Activated = true;
+    }
+
+    public void EndAttack()
+    {
+        print("Attack End!");
+        laserScript.Activated = false;
+        laserScript.PlayParticles(false);
+    }
+}
