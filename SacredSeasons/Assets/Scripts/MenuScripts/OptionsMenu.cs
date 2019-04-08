8d1dd85cb475ba869ed1d9314e918e8801e64522
Mr-Nobody99 <Kutzy99@gmail.com>
2019-04-08 16:07:52 -0400

    Copying Thomas's scripts over

---
+++ SacredSeasons/Assets/Scripts/MenuScripts/OptionsMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public Slider Sound;
+
+    public void Back()
+    {
+        UIManager.instance.ShowScreen("Main Menu");
+    }
+
+    public void Graphics()
+    {
+        UIManager.instance.ShowScreen("Graphics Menu");
+    }
+
+    public void SetAudioVolume(float x)
+    {
+        Debug.Log("Setting Audio Volume to: " + x);
+        GameManager.instance.audioVolume = x;
+    }
+
+    void Start()
+    {
+        Sound.value = GameManager.instance.audioVolume;
+    }
+}
---
+++ SacredSeasons/Assets/Scripts/MenuScripts/PauseMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+   public void Back()
+    {
+        UIManager.instance.Play();
+    }
+
+    public void Options()
+    {
+        UIManager.instance.ShowScreen("Options Menu");
+    }
+
+    public void Quit()
+    {
+        UIManager.instance.Quit();
+    }
+}
