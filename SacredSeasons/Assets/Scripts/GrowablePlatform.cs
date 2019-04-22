9edaa22c16b441383977a19ded1fd8c4ebb16686
Puppetmaster909 <pringthomas4@gmail.com>
2019-04-22 06:03:22 -0400

    Fixed things? It's getitng late...Started playing with growth mechanic

--- SacredSeasons/Assets/Scripts/GrowablePlatform.cs
+++ SacredSeasons/Assets/Scripts/GrowablePlatform.cs
@@ -18,17 +18,21 @@ public class GrowablePlatform : MonoBehaviour
 
     [HideInInspector]
     public bool doGrow = false;
+    [HideInInspector]
+    public bool doShrink = false;
 
     Vector3 initialScale;
     Vector3 targetScale;
 
+    bool grown = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        platformText.SetActive(false);
+        //platformText.SetActive(false);
         initialScale = new Vector3(1f, 0f, 1f);
         targetScale = new Vector3(1f, growHeight, 1f);
-        platform.SetActive(false);
+        //platform.SetActive(false);
         platform.transform.localScale = new Vector3(1f, 0f, 1f);
     }
 
@@ -41,10 +45,36 @@ public class GrowablePlatform : MonoBehaviour
             if (platform.transform.localScale.y >= targetScale.y - 0.15f)
             {
                 doGrow = false;
+                grown = true;
+            }
+        }
+        else if (doShrink)
+        {
+            platform.transform.localScale = Vector3.Lerp(platform.transform.localScale, initialScale, Time.deltaTime * growSpeed);
+            if (platform.transform.localScale.y >= initialScale.y - 0.15f)
+            {
+                doShrink = false;
+                grown = false;
+                ps.Play();
             }
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Earth Projectile") && !grown)
+        {
+            doGrow = true;
+            //platform.SetActive(true);
+        }
+        else if (other.tag.Equals("Earth projectile") && grown)
+        {
+            doShrink = true;
+        }
+    }
+
+    /*
+    // Take out, has to work on spellcast
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag.Equals("Player"))
@@ -53,6 +83,7 @@ public class GrowablePlatform : MonoBehaviour
         }
     }
 
+    // Take out, has to grow/shrink on toggle based on spell cast
     private void OnTriggerStay(Collider other)
     {
         if(other.tag.Equals("Player") && Input.GetButtonDown("Interact"))
@@ -62,11 +93,12 @@ public class GrowablePlatform : MonoBehaviour
         }
     }
 
+    // Take out, see above reasons ^
     private void OnTriggerExit(Collider other)
     {
         if(other.tag.Equals("Player"))
         {
             platformText.SetActive(false);
         }
-    }
+    }*/
 }
