2d6cce4dea7143b55e3bd613006963bfbafe308b
Puppetmaster909 <pringthomas4@gmail.com>
2019-04-28 02:07:11 -0400

    Revert "Merge branch 'master' into Thomas-New"
    This reverts commit e765320cac94fda70ac4c0a151871915db16230b, reversing
    changes made to 6d72cdf376e49f2c5b7c3088c76e6bc9c757431c.

--- SacredSeasons/Assets/Scripts/MenuScripts/OptionsMenu.cs
+++ SacredSeasons/Assets/Scripts/MenuScripts/OptionsMenu.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
-    public Slider Sound;
+    public Slider BGM;
+    public Slider SFX;
 
     public void Back()
     {
@@ -19,10 +20,16 @@ public class OptionsMenu : MonoBehaviour
         UIManager.instance.ShowScreen("Graphics Menu");
     }
 
-    public void SetAudioVolume()
+    public void SetBGMVolume()
     {
-        Debug.Log("Setting Audio Volume to: " + Sound.value);
-        AudioManager.instance.PlaySingle(UIManager.instance.confirm);
+        //Debug.Log("Setting BGM Volume to: " + Sound.value);
+        AudioManager.instance.mixer.SetFloat("bgmVolume", BGM.value);
+        //AudioManager.instance.PlaySingle(UIManager.instance.confirm);
+    }
+
+    public void SetSFXVolume()
+    {
+        AudioManager.instance.mixer.SetFloat("sfxVolume", SFX.value);
     }
 
     void Start()
