9c26235c253d394d2d25738787c84c683daa45c0
Puppetmaster909 <pringthomas4@gmail.com>
2019-04-28 23:07:28 -0400

    Edited spring collectible prefabs

---
+++ SacredSeasons/Assets/Scripts/ScoreCollectible.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCollectible : MonoBehaviour
+{
+    [SerializeField]
+    int value = 5;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerScore.AddToCurrentScore(value);
+            Destroy(gameObject);
+        }
+    }
+}
